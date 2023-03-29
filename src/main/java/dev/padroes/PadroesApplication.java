319f7dad11bbc404b09f696cf40f370515bc8656
moisesmedeiros7 <moisesmedeiros@live.com>
2023-03-29 15:17:35 -0300

    Initial commit

---
+++ src/main/java/dev/padroes/PadroesApplication.java
@@ -0,0 +1,13 @@
+package dev.padroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesApplication.class, args);
+	}
+
+}
